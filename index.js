819101e6dcb84aa3878d63748e580c31d36c545e
Aaron <adgholson98@gmail.com>
2023-07-18 13:53:21 -0600

    completed lab

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Write your code here!
+const ul =  document.querySelector('main#main');
+ul.remove();
+
+const newHeader = document.createElement("h1");
+newHeader.setAttribute("id", "victory");
+newHeader.textContent = "Aaron is the champion";
